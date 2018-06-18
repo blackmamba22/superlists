c6854064ebfbfefc20066b5da526da44e97f99cc
Gawaine O'Gilvie <gawaine.ogilvie@gmail.com>
2018-06-18 12:48:49 -0400

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -1,25 +1,8 @@
 from django.test import TestCase
-from django.urls import resolve
-
-from lists.views import home_page
 from lists.models import Item
 
 
 class HomePageTest(TestCase):
-
-    # def test_root_url_resolves_to_home_page_view(self):
-    #     found = resolve('/')
-    #     self.assertEqual(found.func, home_page)
-
-    # def test_home_page_returns_correct_html(self):
-    #     response = self.client.get('/')
-    #
-    #     html = response.content.decode('utf8')
-    #     self.assertTrue(html.startswith('<html>'))
-    #     self.assertIn('<title>To-Do lists</title>', html)
-    #     self.assertTrue(html.strip().endswith('</html>'))
-    #
-    #     self.assertTemplateUsed(response, 'home.html')
 
     def test_uses_home_template(self):
         response = self.client.get('/')
@@ -35,20 +18,29 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'],
+                         '/lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_displays_all_list_items(self):
+
+class LiveViewTest(TestCase):
+
+    def test_uses_list_templates(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_display_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
 
 
 class ItemModelTest(TestCase):
